90d0963f8cae88ff706063b1c6bc84f213afdda1
Qiang Wang <wang-q@outlook.com>
2026-02-03 01:34:25 +0800

    Update mod.rs

--- src/cmd_pgr/psl/mod.rs
+++ src/cmd_pgr/psl/mod.rs
@@ -7,6 +7,12 @@ pub mod tochain;
 pub fn make_subcommand() -> clap::Command {
     clap::Command::new("psl")
         .about("Psl tools")
+        .after_help(
+            r###"Note:
+These utilities are primarily provided for cross-validation with the original UCSC Kent tools,
+ensuring the fidelity of the ported libraries.
+"###,
+        )
         .subcommand(histo::make_subcommand())
         .subcommand(rc::make_subcommand())
         .subcommand(stats::make_subcommand())
