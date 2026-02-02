0f432a619b4aa3286661265b75423a81f9a33a44
Qiang Wang <wang-q@outlook.com>
2026-02-02 08:33:45 +0800

    pgr fa n50

--- src/cmd_pgr/fa/mod.rs
+++ src/cmd_pgr/fa/mod.rs
@@ -1,3 +1,4 @@
+pub mod n50;
 pub mod size;
 pub mod some;
 pub mod to2bit;
@@ -5,6 +6,7 @@ pub mod to2bit;
 pub fn make_subcommand() -> clap::Command {
     clap::Command::new("fa")
         .about("Fasta tools")
+        .subcommand(n50::make_subcommand())
         .subcommand(size::make_subcommand())
         .subcommand(some::make_subcommand())
         .subcommand(to2bit::make_subcommand())
@@ -12,6 +14,7 @@ pub fn make_subcommand() -> clap::Command {
 
 pub fn execute(matches: &clap::ArgMatches) -> anyhow::Result<()> {
     match matches.subcommand() {
+        Some(("n50", sub_matches)) => n50::execute(sub_matches),
         Some(("size", sub_matches)) => size::execute(sub_matches),
         Some(("some", sub_matches)) => some::execute(sub_matches),
         Some(("to2bit", sub_matches)) => to2bit::execute(sub_matches),
--- tests/cli_fa.rs
+++ tests/cli_fa.rs
@@ -113,3 +113,156 @@ fn command_fa_some_invert() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn command_fa_n50() -> anyhow::Result<()> {
+    let temp = TempDir::new()?;
+    let input = temp.path().join("test.fa");
+
+    // 100, 100, 200, 200, 400
+    // Total = 1000
+    // N50 = 200 (at 500, we have 400+200 >= 500)
+    fs::write(
+        &input,
+        ">seq1\nN\n>seq2\nN\n>seq3\nNN\n>seq4\nNN\n>seq5\nNNNN\n"
+            .replace("N", "N".repeat(100).as_str()),
+    )?;
+
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd.arg("fa").arg("n50").arg(&input).output()?;
+
+    let stdout = String::from_utf8(output.stdout)?;
+    assert!(stdout.contains("N50\t200\n"));
+
+    Ok(())
+}
+
+#[test]
+fn command_fa_n50_stats() -> anyhow::Result<()> {
+    let temp = TempDir::new()?;
+    let input = temp.path().join("test.fa");
+
+    fs::write(
+        &input,
+        ">seq1\nN\n>seq2\nN\n>seq3\nNN\n>seq4\nNN\n>seq5\nNNNN\n"
+            .replace("N", "N".repeat(100).as_str()),
+    )?;
+
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg(&input)
+        .arg("-S")
+        .arg("-A")
+        .arg("-C")
+        .arg("-H")
+        .output()?;
+
+    let stdout = String::from_utf8(output.stdout)?;
+    // N50
+    assert!(stdout.contains("200\n"));
+    // Sum
+    assert!(stdout.contains("1000\n"));
+    // Avg
+    assert!(stdout.contains("200.00\n"));
+    // Count
+    assert!(stdout.contains("5\n"));
+
+    Ok(())
+}
+
+
+#[test]
+fn command_fa_n50_comprehensive() -> anyhow::Result<()> {
+    // display header
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd.arg("fa").arg("n50").arg("tests/fasta/ufasta.fa").output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(stdout.contains("N50\t314"), "line 1");
+
+    // doesn't display header
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg("tests/fasta/ufasta.fa")
+        .arg("-H")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(!stdout.contains("N50\t314"), "line 1");
+    assert!(stdout.contains("314"), "line 1");
+
+    // set genome size (NG50)
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg("tests/fasta/ufasta.fa")
+        .arg("-H")
+        .arg("-g")
+        .arg("10000")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(stdout.contains("297"), "line 1");
+
+    // sum and average of size
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg("tests/fasta/ufasta.fa")
+        .arg("-H")
+        .arg("-S")
+        .arg("-A")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 3);
+    assert!(stdout.contains("314\n9317\n186.34"), "line 1,2,3");
+
+    // N10, N90, E-size
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg("tests/fasta/ufasta.fa")
+        .arg("-H")
+        .arg("-E")
+        .arg("-N")
+        .arg("10")
+        .arg("-N")
+        .arg("90")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 3);
+    assert!(stdout.contains("516\n112\n314.70\n"), "line 1,2,3");
+
+    // transposed
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("fa")
+        .arg("n50")
+        .arg("tests/fasta/ufasta.fa")
+        .arg("-E")
+        .arg("-N")
+        .arg("10")
+        .arg("-N")
+        .arg("90")
+        .arg("-t")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 2);
+    assert!(stdout.contains("N10\tN90\tE\n"), "line 1");
+    assert!(stdout.contains("516\t112\t314.70\n"), "line 2");
+
+    Ok(())
+}
