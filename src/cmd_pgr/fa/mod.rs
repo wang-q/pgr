3f2e4137c5485bb7feff122bae18552bbcf1a0f0
Qiang Wang <wang-q@outlook.com>
2026-02-02 08:19:57 +0800

    pgr fa some

--- src/cmd_pgr/fa/mod.rs
+++ src/cmd_pgr/fa/mod.rs
@@ -1,16 +1,19 @@
 pub mod size;
+pub mod some;
 pub mod to2bit;
 
 pub fn make_subcommand() -> clap::Command {
     clap::Command::new("fa")
         .about("Fasta tools")
         .subcommand(size::make_subcommand())
+        .subcommand(some::make_subcommand())
         .subcommand(to2bit::make_subcommand())
 }
 
 pub fn execute(matches: &clap::ArgMatches) -> anyhow::Result<()> {
     match matches.subcommand() {
         Some(("size", sub_matches)) => size::execute(sub_matches),
+        Some(("some", sub_matches)) => some::execute(sub_matches),
         Some(("to2bit", sub_matches)) => to2bit::execute(sub_matches),
         _ => Ok(()),
     }
