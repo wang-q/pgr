0d04ec237929ef73b4e0b72bf0a0a9561616e08d
Qiang Wang <wang-q@outlook.com>
2026-02-05 08:01:18 +0800

    Moved from hnsm

--- src/cmd_pgr/dist/mod.rs
+++ src/cmd_pgr/dist/mod.rs
@@ -1,4 +1,5 @@
 pub mod hv;
+pub mod seq;
 pub mod vector;
 
 pub fn make_subcommand() -> clap::Command {
@@ -7,19 +8,21 @@ pub fn make_subcommand() -> clap::Command {
         .after_help(
             r###"Subcommand groups:
 
-* distance: hv / vector
+* distance: hv / seq / vector
 
 "###,
         )
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(hv::make_subcommand())
+        .subcommand(seq::make_subcommand())
         .subcommand(vector::make_subcommand())
 }
 
 pub fn execute(matches: &clap::ArgMatches) -> anyhow::Result<()> {
     match matches.subcommand() {
         Some(("hv", sub_matches)) => hv::execute(sub_matches),
+        Some(("seq", sub_matches)) => seq::execute(sub_matches),
         Some(("vector", sub_matches)) => vector::execute(sub_matches),
         _ => Ok(()),
     }
--- tests/cli_dist.rs
+++ tests/cli_dist.rs
@@ -58,3 +58,94 @@ fn command_dist_vector() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn command_dist_seq() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("dist")
+        .arg("seq")
+        .arg("tests/clust/IBPA.fa")
+        .arg("-k")
+        .arg("7")
+        .arg("-w")
+        .arg("1")
+        .arg("--zero")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 100);
+    assert!(stdout.contains("IBPA_ECOLI\tIBPA_ECOLI_GA\t0.0669\t0.4556\t0.6260"));
+
+    Ok(())
+}
+
+#[test]
+fn command_dist_seq_sim() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("dist")
+        .arg("seq")
+        .arg("tests/clust/IBPA.fa")
+        .arg("-k")
+        .arg("7")
+        .arg("-w")
+        .arg("1")
+        .arg("--zero")
+        .arg("--sim")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 100);
+    // Mash dist 0.0669 -> Sim 1 - 0.0669 = 0.9331
+    assert!(stdout.contains("IBPA_ECOLI\tIBPA_ECOLI_GA\t0.9331\t0.4556\t0.6260"));
+
+    Ok(())
+}
+
+#[test]
+fn command_dist_seq_genome() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("dist")
+        .arg("seq")
+        .arg("tests/genome/sakai.fa.gz")
+        .arg("tests/genome/mg1655.fa.gz")
+        .arg("-k")
+        .arg("21")
+        .arg("-w")
+        .arg("5")
+        .arg("--hasher")
+        .arg("mod")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 2);
+    assert!(stdout.contains("NC_002695\tNC_000913\t0."));
+    assert!(stdout.contains("NC_002128\tNC_000913\t0."));
+
+    Ok(())
+}
+
+#[test]
+fn command_dist_seq_merge() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgr")?;
+    let output = cmd
+        .arg("dist")
+        .arg("seq")
+        .arg("tests/clust/IBPA.fa")
+        .arg("-k")
+        .arg("7")
+        .arg("-w")
+        .arg("1")
+        .arg("--merge")
+        .arg("--hasher")
+        .arg("murmur")
+        .output()?;
+    let stdout = String::from_utf8(output.stdout)?;
+
+    assert_eq!(stdout.lines().count(), 1);
+    assert!(stdout.contains("tests/clust/IBPA.fa\ttests/clust/IBPA.fa\t763"));
+
+    Ok(())
+}
