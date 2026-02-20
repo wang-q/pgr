cad859672b4407cc1194ee372db5ae3127906d8a
Qiang Wang <wang-q@users.noreply.github.com>
2026-02-21 01:30:36 +0800

    Create cli_fas_multiz.rs

---
+++ tests/cli_fas_multiz.rs
@@ -0,0 +1,37 @@
+use assert_cmd::cargo::cargo_bin_cmd;
+use std::fs;
+use tempfile::TempDir;
+
+#[test]
+fn command_fas_multiz_core() -> anyhow::Result<()> {
+    let tempdir = TempDir::new()?;
+    let out_path = tempdir.path().join("merged.fas");
+    let out_str = out_path.to_str().unwrap();
+
+    let mut cmd = cargo_bin_cmd!("pgr");
+    let output = cmd
+        .arg("fas")
+        .arg("multiz")
+        .arg("-r")
+        .arg("S288c")
+        .arg("tests/fas/S288cvsRM11_1a.slice.fas")
+        .arg("tests/fas/S288cvsYJM789.slice.fas")
+        .arg("tests/fas/S288cvsSpar.slice.fas")
+        .arg("-o")
+        .arg(out_str)
+        .output()?;
+
+    if !output.status.success() {
+        let stderr = String::from_utf8(output.stderr)?;
+        panic!("pgr fas multiz failed: {}", stderr);
+    }
+
+    assert!(out_path.is_file());
+    let content = fs::read_to_string(out_path)?;
+    assert!(content.lines().count() > 0);
+
+    tempdir.close()?;
+
+    Ok(())
+}
+
