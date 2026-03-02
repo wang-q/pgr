26b5299743f244c1e5215e4c6633fc7ed3ec0966
Qiang Wang <wang-q@outlook.com>
2026-03-03 02:30:12 +0800

    plot hh

--- src/cmd_pgr/plot/mod.rs
+++ src/cmd_pgr/plot/mod.rs
@@ -1,16 +1,19 @@
 use clap::Command;
 
+pub mod hh;
 pub mod venn;
 
 pub fn make_subcommand() -> Command {
     Command::new("plot")
         .about("Plotting tools")
         .subcommand_required(true)
+        .subcommand(hh::make_subcommand())
         .subcommand(venn::make_subcommand())
 }
 
 pub fn execute(matches: &clap::ArgMatches) -> anyhow::Result<()> {
     match matches.subcommand() {
+        Some(("hh", sub_matches)) => hh::execute(sub_matches),
         Some(("venn", sub_matches)) => venn::execute(sub_matches),
         _ => unreachable!(),
     }
--- tests/cli_plot.rs
+++ tests/cli_plot.rs
@@ -51,3 +51,28 @@ fn command_plot_venn4() {
     assert!(stdout.contains("(-2.2, -2.6) { rocauc }"));
     assert!(stdout.contains("(-2.2,  1.5) { 161 }"));
 }
+
+#[test]
+fn command_plot_hh() {
+    let (stdout, _) = PgrCmd::new()
+        .args(&["plot", "hh", "tests/plot/hist.tsv"])
+        .run();
+
+    assert!(stdout.contains("31   0 0.0200"));
+    assert!(stdout.contains("31   1 0.0000"));
+
+    let (stdout, _) = PgrCmd::new()
+        .args(&[
+            "plot",
+            "hh",
+            "tests/plot/hist.tsv",
+            "-g",
+            "2",
+            "--bins",
+            "20",
+        ])
+        .run();
+
+    assert!(stdout.contains("11   0 0.0600"));
+    assert!(stdout.contains("11   1 0.1600"));
+}
